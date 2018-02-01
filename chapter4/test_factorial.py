4399d87da724d2eb6f896d79fdfc4ea5e605e86c
Lucas Cavalare <lucascavalare@gmail.com>
2018-02-01 17:36:54 -0200

    Rolling back function

--- chapter4/test_factorial.py
+++ chapter4/test_factorial.py
@@ -3,10 +3,10 @@ from factorial import fac
 
 class Test(unittest.TestCase):
 	
-	#def test_number(self):
-	#	self.assertEqual(24,fac(4))
-	#	self.assertEqual(120,fac(5))
-	#	self.assertEqual(720,fac(6))
+	def test_number(self):
+		self.assertEqual(24,fac(4))
+		self.assertEqual(120,fac(5))
+		self.assertEqual(720,fac(6))
 
 	def test_zero(self):
 		self.assertEqual(1,fac(0))
